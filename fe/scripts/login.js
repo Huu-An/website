d7c72191af135fce5b942ba64e5e5723e1f06f07
Nguyen Huu An <annh@AnNH.local>
2025-02-27 15:56:13 +0700

    update login

--- fe/scripts/login.js
+++ fe/scripts/login.js
@@ -1,7 +1,7 @@
 const CLIENT_ID = "298892762962-ul9g91b1s2jb9ne2uobhl2te2h07n0br.apps.googleusercontent.com";
 const CLIENT_SECRET = "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";
-const URL_TOKEN = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.email%20https%3A//www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=http://127.0.0.1:5500/index.html&client_id=${CLIENT_ID}`;
-const ROOT = "127.0.0.1:5500";
+const URL_TOKEN = `https://accounts.google.com/o/oauth2/v2/auth?scope=https%3A//www.googleapis.com/auth/userinfo.email%20https%3A//www.googleapis.com/auth/userinfo.profile&response_type=token&redirect_uri=https://hanglong.xyz/&client_id=${CLIENT_ID}`;
+const ROOT = "http://157.66.81.167";
 
 const checkLogin = () => {
   if (document.cookie != "") {
